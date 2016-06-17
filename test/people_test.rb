888065f18403dbe7ae7b3ee922704025e544c6ed
Scott Sanders <jssjr@github.com>
2016-06-17 12:43:19 -0400

    Validate the people data can be coerced to a hash

--- test/people_test.rb
+++ test/people_test.rb
@@ -1,6 +1,14 @@
 require_relative "test_helper"
 
 class PeopleTest < ChsTech::Test
+  # Ensure the people profile YAML documents can be coerced into a hash
+  def test_profile_is_valid_yaml
+    people_files.each do |filename|
+      assert_respond_to YAML.load_file(filename), :to_h, "#{filename} isn't valid people data"
+    end
+  end
+
+  # Ensure supplied GitHub usernames are real
   def test_github_profile_exists
     people.each do |name, info|
       github_username = info.fetch("github", false)
